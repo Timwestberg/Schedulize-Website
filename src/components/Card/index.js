5d50d9f68efdeb2d286ef4f5a8bfbbf45400b43d
ptamayo <ptamayo97@yahoo.com>
2019-02-19 21:39:48 -0800

    pricing skeleton done

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,8 +2,21 @@ import React from "react";
 
 const Card = props => {
   return (
-    <div className="card">
-      
+    <div className='card'>
+      <div className='card-header title level is-mobile'>
+        <div className='level-item'>{props.name}</div>
+      </div>
+      <div className=' is-multiline card-content'>
+        <div className='column is-full level'>
+          <h1 className='title level-item'>{props.price}</h1>
+        </div>
+        <div className='column is-full'>
+          <p>
+            Dolore do non sunt deserunt ipsum irure aute voluptate. Nisi ut
+            ipsum aliqua ipsum.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -2,24 +2,34 @@ import React from "react";
 import Card from "../components/Card";
 
 class Pricing extends React.Component {
-  state = {};
+  state = {
+    cards: [
+      {
+        id: 1,
+        name: "Small Business",
+        price: "$99/mo",
+        description: ""
+      },
+      { id: 2, name: "Custom", price: "10k", description: "" }
+    ]
+  };
 
   render() {
     return (
       //   <div className="hero-body has-background-dark">
       <div className='container is-fluid'>
-        <section className='section has-text-centered '>
-          <h1 className='title has-text-white'>Pricing</h1>
+        <section className='box has-text-centered has-background-black'>
+          <h1 className='title has-text-white'>Pricing </h1>
         </section>
-        {/* <section className="section ">
-            <div className=" columns is-multiline">
-              {this.state.cards.map(card => (
-                <div className="column is-3">
-                  <Card key={card.id} name={card.name} img={card.img} />
-                </div>
-              ))}
-            </div>
-          </section> */}
+        <section className='section '>
+          <div className='columns '>
+            {this.state.cards.map(card => (
+              <div className='column'>
+                <Card key={card.id} name={card.name} price={card.price} />
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
       //   </div>
     );
